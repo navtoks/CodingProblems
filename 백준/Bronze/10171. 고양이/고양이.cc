2a74c3c984ba20a775155b1c0e18b15bf592577b
navtoks <137356007+navtoks@users.noreply.github.com>
2023-06-22 08:04:12 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << R"(\    /\)" << '\n';
+    cout << R"( )  ( '))" << '\n';
+    cout << R"((  /  ))" << '\n';
+    cout << R"( \(__)|)";
+}
