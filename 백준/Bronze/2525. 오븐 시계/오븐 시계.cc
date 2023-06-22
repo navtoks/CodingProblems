3080279db8dee7336723ea0975e53ae3c4ae966d
navtoks <137356007+navtoks@users.noreply.github.com>
2023-06-23 07:59:46 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    b += c;
+    a += (b / 60);
+    if (a > 23) a %= 24;
+    b %= 60;
+    cout << a << ' ' << b;
+}
