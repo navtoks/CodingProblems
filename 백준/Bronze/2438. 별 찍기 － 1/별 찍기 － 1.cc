55769085869dc9d21eae5bd3d6ab301c8e470434
navtoks <137356007+navtoks@users.noreply.github.com>
2023-06-25 17:28:03 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) cout << '*';
+        cout << '\n';
+    }
+}
