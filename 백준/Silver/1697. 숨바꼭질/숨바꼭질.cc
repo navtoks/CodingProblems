5ceecc5a1746be4f9e2f2ea0f3bc8d706f92303d
navtoks <137356007+navtoks@users.noreply.github.com>
2023-07-26 19:52:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2936 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int MAXN = 100'000;
+vector<int> arr(MAXN * 2 + 1, -1);
+
+void NextMove(int u, int v, queue<int>& checker) {
+    if (v > MAXN || v < 0) return;
+    if (arr[v] != -1) return;
+    arr[v] = arr[u] + 1;
+    checker.push(v);
+}
+
+void Bfs(int start) {
+    queue<int> temp;
+    int u = start;
+    temp.push(u);
+    arr[u] = 0;
+    while (!temp.empty()) {
+        u = temp.front();
+        temp.pop();
+        NextMove(u, u + 1, temp);
+        NextMove(u, u - 1, temp);
+        NextMove(u, 2 * u, temp);
+    }
+}
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    int n, k;
+    cin >> n >> k;
+
+    Bfs(n);
+    cout << arr[k];
+
+    return 0;
+}
