593b63af4039492e8ceefb3b3326bd0ff45afbf1
navtoks <137356007+navtoks@users.noreply.github.com>
2023-08-31 02:08:02 +0900

    [Silver I] Title: 가장 가까운 세 사람의 심리적 거리, Time: 12 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
+++ 백준/Silver/20529. 가장 가까운 세 사람의 심리적 거리/가장 가까운 세 사람의 심리적 거리.cc
@@ -4,17 +4,17 @@
 #include <unordered_map>
 using namespace std;
 using vi = vector<int>;
-using dict1 = unordered_map<string, int>;
-using dict2 = unordered_map<int, string>;
 
-dict1 map_to_num;
-dict2 map_to_str;
+#define FOR(i) for(int i = 0; i < 16; ++i)
+
+unordered_map<string, int> map_to_num;
+unordered_map<int, string> map_to_str;
 vector<vi> dists(16, vi(16, 0));
 vi arr;
 
 void SetMaps() {
     string s{"0000"};
-    for (int i = 0; i < 16; ++i) {
+    FOR(i) {
         s[0] = (i & 1 ? 'E' : 'I');
         s[1] = (i & 2 ? 'S' : 'N');
         s[2] = (i & 4 ? 'T' : 'F');
@@ -26,33 +26,34 @@ void SetMaps() {
 }
 
 void SetDists() {
-    for (int i = 0; i < 16; ++i) {
-        for (int j = 0; j < 16; ++j) {
-            const auto& s1 = map_to_str[i];
-            const auto& s2 = map_to_str[j];
-            for (int k = 0; k < 4; ++k) {
-                if (s1[k] != s2[k]) ++dists[i][j];
+    FOR(i) {
+        FOR(j) {
+            if (i == j) continue;
+            if (j < i) dists[i][j] = dists[j][i];
+            else {
+                const auto& s1 = map_to_str[i];
+                const auto& s2 = map_to_str[j];
+                for (int k = 0; k < 4; ++k) {
+                    if (s1[k] != s2[k]) ++dists[i][j];
+                }
             }
         }
     }
 }
 
 void Case2(int i, int& result) {
-    for (int j = 0; j < 16; ++j) {
+    FOR(j) {
         if (arr[j] == 0 || i == j) continue;
-        int dist = dists[i][j] * 2;
-        if (dist < result) result = dist;
+        result = min(result, dists[i][j] * 2);
     }
 }
 
 void Case3(int i, int& result) {
-    for (int j = 0; j < 16; ++j) {
+    FOR(j) {
         if (arr[j] == 0 || i == j) continue;
-
-        for (int k = 0; k < 16; ++k) {
+        FOR(k) {
             if (arr[k] == 0 || i == k || j == k) continue;
-            int dist = dists[i][j] + dists[i][k] + dists[j][k];
-            if (dist < result) result = dist;
+            result = min(result, dists[i][j] + dists[i][k] + dists[j][k]);
         }
     }
 }
@@ -63,25 +64,26 @@ int main() {
     SetMaps();
     SetDists();
 
-    int T;
+    int T, n;
     cin >> T;
     while (T--) {
-        int n;
         cin >> n;
 
         arr = vi(16, 0);
-        for (int i = 0; i < n; ++i) {
+        while (n--) {
             string s;
             cin >> s;
             ++arr[map_to_num[s]];
         }
 
         int result = 12;
-        for (int i = 0; i < 16; ++i) {
-            if (arr[i] == 0) continue;
-            if (arr[i] > 2) result = 0;
+        FOR(i) {
+            if (arr[i] > 2) {
+                result = 0;
+                break;
+            }
             else if (arr[i] == 2) Case2(i, result);
-            else Case3(i, result);
+            else if (arr[i]) Case3(i, result);
         }
         cout << result << '\n';
     }
