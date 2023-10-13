6e16545e60d4062f8695be244ca7b59363e56bb8
navtoks <137356007+navtoks@users.noreply.github.com>
2023-10-13 23:32:34 +0900

    [Gold V] Title: 리벤지 오브 피타고라스, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/4563. 리벤지 오브 피타고라스/리벤지 오브 피타고라스.cc
+++ 백준/Gold/4563. 리벤지 오브 피타고라스/리벤지 오브 피타고라스.cc
@@ -1,17 +1,6 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 using ll = long long;
-
-vector<ll> FindDivs(ll n) {
-    vector<ll> divs;
-    for (ll i = 1; i * i <= n; ++i) {
-        if (n % i) continue;
-        divs.push_back(i);
-        divs.push_back(n / i);
-    }
-    return divs;
-}
 
 int main() {
     cin.tie(nullptr);   ios_base::sync_with_stdio(false);
@@ -21,12 +10,16 @@ int main() {
         cin >> a;
         if (a == 0) break;
 
+
         int cnt = 0;
-        auto divs = FindDivs(a * a);
-        for (int i = 0; i < divs.size(); i += 2) {
-            ll c2 = divs[i] + divs[i + 1];
-            ll b2 = divs[i + 1] - divs[i];
-            if (c2 % 2 == 0 && b2 % 2 == 0 && b2 / 2 > a) ++cnt;
+        ll n = a * a;
+        for (ll i = 1; i <= a; ++i) {
+            if (n % i) continue;
+            ll j = n / i;
+            ll c2 = i + j, b2 = j - i;
+            if (b2 <= 2 * a) break;
+            if ((c2 & 1) && (b2 & 1)) continue;
+            ++cnt;
         }
 
         cout << cnt << '\n';
