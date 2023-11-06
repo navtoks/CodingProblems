648392014c3c9b831f5f7d7bfdb2a2fdeab3c7a2
navtoks <137356007+navtoks@users.noreply.github.com>
2023-11-07 00:29:03 +0900

    [Silver III] Title: 바이너리 왕국, Time: 236 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/16567. 바이너리 왕국/바이너리 왕국.cc
+++ 백준/Silver/16567. 바이너리 왕국/바이너리 왕국.cc
@@ -10,7 +10,7 @@ int main() {
 
     int cnt = 0;
     bool cont = false;
-    vector<int> arr(n + 1);
+    vector<int> arr(n + 2, 0);
     for (int i = 1; i <= n; ++i) {
         cin >> arr[i];
         if (arr[i]) {
@@ -27,10 +27,7 @@ int main() {
         if (x) {
             cin >> x;
             if (arr[x]) continue;
-
-            if (x == 1) cnt += (1 - arr[2]);
-            else if (x == n) cnt += (1 - arr[n - 1]);
-            else cnt += (1 - arr[x - 1] - arr[x + 1]);
+            cnt += (1 - arr[x - 1] - arr[x + 1]);
             arr[x] = 1;
         }
         else cout << cnt << '\n';
