625107b36807f97dc200969bf26dd7c328c21a54
navtoks <137356007+navtoks@users.noreply.github.com>
2023-12-01 23:49:00 +0900

    [Silver III] Title: 2 타워, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10407. 2 타워/2 타워.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+
+    if (s == "1") cout << 2;
+    else cout << 1;
+    
+    return 0;
+}
