c13b024ab10e63e0670b258e76715cb3964eb1df
navtoks <137356007+navtoks@users.noreply.github.com>
2024-03-16 14:09:24 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 4 ms, Memory: 2516 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);   ios_base::sync_with_stdio(false);
+
+    string s;
+    getline(cin, s);
+    int n = s.size();
+
+    int i = 0;
+    vector<string> arr;
+    while (i < n) {
+        if (s[i] == '<') {
+            arr.push_back("");
+            while (s[i] != '>' && i < n) arr.back() += s[i++];
+            arr.back() += s[i++];
+        }
+        else if (s[i] != ' ') {
+            arr.push_back("");
+            while (s[i] != ' ' && s[i] != '<' && i < n) arr.back() += s[i++];
+        }
+        else {  // s[i] == ' '
+            ++i;
+            arr.push_back(" ");
+            while (s[i] != ' ' && s[i] != '<' && i < n) arr.back() += s[i++];
+        }
+    }
+
+    for (const auto& elt : arr) {
+        if (elt[0] == '<') cout << elt;
+        else {
+            n = 0;
+            if (elt[0] == ' ') {
+                cout << ' ';
+                ++n;
+            }
+            for (i = elt.size() - 1; i >= n; --i) cout << elt[i];
+        }
+    }
+
+    return 0;
+}
